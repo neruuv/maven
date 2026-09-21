4c3cf5a1be3693fd74fe44d74832dd984e31cb42
neruuv <nerea.ballesters@gmail.com>
2026-09-21 18:40:34 +0200

    Prueba

---
+++ PruebaMaven/src/main/java/es/um/informatica/PruebaMaven/App.java
@@ -0,0 +1,10 @@
+package es.um.informatica.PruebaMaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
